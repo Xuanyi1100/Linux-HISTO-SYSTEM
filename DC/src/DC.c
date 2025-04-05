235445ad8d011a20373bc2f3b82a47b0423f7049
Xuan Yi <davidronndio@gmail.com>
2025-04-05 08:29:26 -0400

    dc done

--- DC/src/DC.c
+++ DC/src/DC.c
@@ -15,23 +15,50 @@ static shared_memory *shm_ptr_global = NULL;
 pid_t dp1_pid;
 pid_t dp2_pid;
 
+volatile sig_atomic_t terminate_flag = 0; // Set by SIGINT handler
+volatile sig_atomic_t display_flag = 0;   // Flag for 10-second display
+int letter_counts[26] = {0};
+const int display_interval = 5;        // Display every 5 * 2 seconds = 10 seconds
+int alarm_counter = 0; 
+
+// display
+void display_histogram()
+{
+    system("clear");  // Clear screen
+    for (int i = 0; i < 20; i++) { // Only 'A' through 'T'
+        char letter = 'A' + i;
+        long long count = letter_counts[i];
+        long long hundreds = count / 100;
+        long long tens = (count % 100) / 10;
+        long long ones = count % 10;
+
+        printf("%c-%03lld ", letter, count); // Print letter and count (e.g., A-058)
+
+        for(int j = 0; j < hundreds; ++j) printf("*");
+        for(int j = 0; j < tens; ++j) printf("+");
+        for(int j = 0; j < ones; ++j) printf("-");
+        printf("\n");
+    }
+    fflush(stdout); // Ensure output is immediately visible
+}
 // --- Signal Handler ---
 void timeToQuit(int sig)
 {
-    // sends SIGINT to DPs,
+    // this handler should only set the flag and call kill. It must not do anything else
     kill(dp1_pid, SIGINT);
     kill(dp2_pid, SIGINT);
+    terminate_flag = 1;
 
-    // reads remaining data,
-    // displays final histogram,
-
-    //  exits with "Shazam!!".
-    printf("Shazam!!\n");
-    exit(0);
 }
 void wakeup_handler(int signum) {
-    printf("Wake-up call! (SIGALRM received)\n");
+
     alarm(2);
+    // display mamagement
+    alarm_counter++;
+    if (alarm_counter >= display_interval) {
+        display_flag = 1;
+        alarm_counter = 0; // Reset counter
+    }
     
 }
 
@@ -81,7 +108,7 @@ int main(int argc, char *argv[])
     // Set the first alarm
     alarm(2);
 
-    while (1)
+    while (!terminate_flag)
     {
         pause();  // Sleep until a signal is received
         // --- Acquire Semaphore ---
@@ -91,21 +118,18 @@ int main(int argc, char *argv[])
             break;
         }
 
-        // read 40 charaters
-        for(int i = 0, i < 40, i++)
+        // Determine how many characters are available to read
+        int available_to_read = (shm_ptr->write_index - shm_ptr->read_index + BUFFER) % BUFFER;
+
+        // read all that can be read
+        for(int i = 0; i < available_to_read; i++)
         {
-            char readChar = shm_ptr_global->buffer[shm_ptr_global->read_index]
-            
+            char c = shm_ptr_global->buffer[shm_ptr_global->read_index];
+            if (c >= 'A' && c <= 'T') {
+                letter_counts[c - 'A']++;
+            }            
             shm_ptr_global->read_index = (shm_ptr_global->read_index + 1) % BUFFER;
-
         }
-
-        // make sure not overtake write index
-
-        // Display every 10 seconds with symbols:
-        // - for ones place,
-        // + for tens place,
-        // * for hundreds place
 
         // Unlock semaphore
         if (semop(sem_id, &unlock, 1) == -1)
@@ -114,8 +138,48 @@ int main(int argc, char *argv[])
             break;
         }
 
-        sleep(10);
+        if(display_flag)
+        {
+            display_histogram();
+            display_flag = 0;
+        }
+
     }
+
+    // --- Acquire Semaphore ---
+        if (semop(sem_id, &lock, 1) == 0)
+        {
+           // Determine how many characters are available to read
+        int available_to_read = (shm_ptr->write_index - shm_ptr->read_index + BUFFER) % BUFFER;
+
+        // read all that can be read
+        for(int i = 0; i < available_to_read; i++)
+        {
+            char c = shm_ptr_global->buffer[shm_ptr_global->read_index];
+            if (c >= 'A' && c <= 'T') {
+                letter_counts[c - 'A']++;
+            }            
+            shm_ptr_global->read_index = (shm_ptr_global->read_index + 1) % BUFFER;
+        }
+        
+        // Release lock (optional, we are removing it anyway)
+         semop(sem_id, &unlock, 1);
+        }
+
+        
+
+
+    // displays final histogram
+    display_histogram();
+
+    // Cleanup IPC
+    shmdt(shm_ptr_global);
+    shmctl(shm_id, IPC_RMID, NULL);
+    semctl(sem_id, 0, IPC_RMID);
+
+    //  exits with "Shazam!!".
+    printf("Shazam!!\n");
+    
 
     return 0;
 }
